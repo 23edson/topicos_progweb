6ac704a54c0fbbf3423498b2bae68fddaaa2b1c6
Edson <ed_edsoon@hotmail.com>
2017-05-16 17:04:34 -0300

    update js

---
+++ src/js/calendar.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+  var date = new Date();
+  var d = date.getDate();
+  var m = date.getMonth();
+  var y = date.getFullYear();
+
+  var calendar = $('#calendar').fullCalendar({
+   editable: true,
+   header: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'month,agendaWeek,agendaDay'
+   },
+   
+   events: "vendors/fullcalendar/test.php",
+   
+   // Convert the allDay from string to boolean
+   eventRender: function(event, element, view) {
+    if (event.allDay === 'true') {
+     event.allDay = true;
+    } else {
+     event.allDay = false;
+    }
+   },
+   selectable: true,
+   selectHelper: true,
+   select: function(start, end, allDay) {
+   var title = prompt('Event Title:');
+   if (title) {
+   	
+   var start = $.fullCalendar.formatDate(start, "yyyy-MM-dd HH:mm:ss");
+   var end = $.fullCalendar.formatDate(end, "yyyy-MM-dd HH:mm:ss");
+   $.ajax({
+   url: 'vendors/fullcalendar/add_events.php',
+   data: 'title='+ title+'&start='+ start +'&end='+ end  ,
+   type: "POST",
+   success: function(json) {
+   alert('Adicionado');
+   }
+   });
+   calendar.fullCalendar('renderEvent',
+   {
+   title: title,
+   start: start,
+   end: end,
+   allDay: allDay
+   },
+   true // make the event "stick"
+   );
+   }
+   calendar.fullCalendar('unselect');
+   },
+   
+   editable: true,
+   eventDrop: function(event, delta) {
+   var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
+   var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
+   $.ajax({
+   url: 'http://localhost:8888/fullcalendar/update_events.php',
+   data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+   type: "POST",
+   success: function(json) {
+    alert("Updated Successfully");
+   }
+   });
+   },
+   eventResize: function(event) {
+   var start = $.fullCalendar.formatDate(event.start, "yyyy-MM-dd HH:mm:ss");
+   var end = $.fullCalendar.formatDate(event.end, "yyyy-MM-dd HH:mm:ss");
+   $.ajax({
+    url: 'http://localhost:8888/fullcalendar/update_events.php',
+    data: 'title='+ event.title+'&start='+ start +'&end='+ end +'&id='+ event.id ,
+    type: "POST",
+    success: function(json) {
+     alert("Updated Successfully");
+    }
+   });
+
+}
+   
+  });
+  
+ });
